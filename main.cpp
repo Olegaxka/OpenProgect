8aa19d4436e0a096ebcbb5459465a31768eae5be
Olegaxka <iliakrulou3223_3@mail.ru>
2024-12-16 17:14:32 +0300

    Dont work (

--- main.cpp
+++ main.cpp
@@ -1,39 +1,17 @@
 //устал -_-(
-
+// Я В ТУПИКЕ ТЕКСТУРА НЕ ОТРИСОВУЕТСЯ
 //ВЖАНО что б glad был с верху заголовочных файлов OpenGl
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
 #include "shader.h"
+#include "stb_image.h"
+
+#include <filesystem>
 
 //функция обратного вызова, вызываеться при каждом изменении окна
 void framebuffer_size_callback(GLFWwindow* window, int width, int height); 
-
-const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
-"layout (location = 1) in vec3 aColor;\n"
-"out vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"   gl_Position = vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
-"}\0";
-
-const char* fragmentShaderSource = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
-"void main()\n"
-"{\n"
-"    FragColor = vec4(ourColor, 1.0);\n"
-"}\0";
-
-const char* fragmentShaderSource_yellow = "#version 330 core\n"
-"out vec4 FragColor;\n"
-"void main()\n"
-"{\n"
-"   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
-"}\0";
 
 
 int main()
@@ -76,35 +54,35 @@ int main()
     Shader ourShader("vertex_shader.glsl", "fragment_shader.glsl");  // путь к файлам шейдеров
 
                              //Указывание вершин (и буферов) и настройка вершинных атрибутов            
-
+    /*
     //координаты вершин треугольника по xyz
     float vertices1[] = {
-     0.1f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // верхняя правая 0
-     0.1f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // нижняя правая  1
-     0.5f,  0.0f, 0.0f,  1.0f, 0.0f, 0.0f   // правая         2 
+    -0.8f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // лево
+     0.8f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // право
+     0.0f,  0.7f, 0.0f,  1.0f, 0.0f, 0.0f   // верх      
+    };
+    */
+
+    float vertices1[] = {
+        // координаты        // цвета            // текстурные координаты
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // верхняя правая
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // нижняя правая
+       -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // нижняя левая
+       -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // верхняя левая 
     };
 
-    //координаты вершин треугольника по xyz
-    float vertices2[] = { 
-    -0.1f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // нижняя левая   3
-    -0.1f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f,  // верхняя левая  4
-    -0.5f,  0.0f, 0.0f,  0.0f, 0.0f, 1.0f   //левая           5 
+                            
+    unsigned int indices[] = {  // помните, что мы начинаем с 0!
+    0, 1, 3, // первый треугольник
+    1, 2, 3  // второй треугольник
     };
-
-    //unsigned int indices[] = {  // помните, что мы начинаем с 0!
-    //0, 1, 2, // первый треугольник
-    //3, 4, 5  // второй треугольник
-    //};
 
     //Обект вершинного буфера объект вершинного буфера имеет тип GL_ARRAY_BUFFER
     //создаем VAO и VBO , EBO
-    unsigned int VBO, VBO1, VAO, VAO1, EBO;
+    unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
     glGenBuffers(1, &EBO);
-
-    glGenVertexArrays(1, &VAO1);
-    glGenBuffers(1, &VBO1);
 
     glBindVertexArray(VAO);
  
@@ -122,17 +100,21 @@ int main()
                  GL_STREAM_DRAW — данные указываются только один раз и используются графическим процессором не более нескольких раз.
                  GL_STATIC_DRAW — данные указываются только один раз и используются много раз.
                  GL_DYNAMIC_DRAW — данные часто изменяются и используются много раз.*/
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
-    // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     //glVertexAttribPointer()- мы сообщаем OpenGL, как он должен интерпретировать данные вершин (для каждого атрибута вершины)
     //Координатные атрибуты
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // Цветовые атрибуты
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
+
+    //текстура
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
 
     // Обратите внимание, что данное действие разрешено, вызов glVertexAttribPointer() зарегистрировал VBO как привязанный вершинный буферный объект
     // для вершинного атрибута, так что после этого мы можем спокойно выполнить отвязку
@@ -142,20 +124,37 @@ int main()
     // Модификация других VAO требует вызова glBindVertexArray(), поэтому мы обычно не снимаем привязку VAO (или VBO), когда это не требуется напрямую
     glBindVertexArray(0);
 
-    glBindVertexArray(VAO1);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(1);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
-
     // Раскомментируйте следующую строку для отрисовки полигонов в режиме каркаса
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
   
+                            //Создание текстуры
+    unsigned int texture;
+    glGenTextures(1, &texture);
+
+    glActiveTexture(GL_TEXTURE0); // сначала активируем текстурный юнит, прежде чем связывать текстуру
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    // Устанавливаем параметры текстуры
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
+    // Загрузка изображения, создание текстуры и генерирование мипмап-уровней
+    int width, height, nrChannels;
+    unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     //цикл рендеринга,
     while (!glfwWindowShouldClose(window))
     {
@@ -168,15 +167,13 @@ int main()
         //функция glClear() использует текущие настройки цвета, установленные при помощи функции glClearColor().
         glClearColor(0.5f, 0.2f, 0.5f, 0.7f);
         glClear(GL_COLOR_BUFFER_BIT);       
-           
+        
         ourShader.use();
+        glBindTexture(GL_TEXTURE_2D, texture);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3); //треугольник
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //квадрат
+        // glDrawArrays(GL_TRIANGLES, 0, 3); //треугольник
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); //квадрат
         // glBindVertexArray(0); // не нужно каждый раз его отвязывать
-
-        glBindVertexArray(VAO1);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // glfw: обмен содержимым front- и back-буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
         glfwSwapBuffers(window);
@@ -186,9 +183,7 @@ int main()
     // Опционально: освобождаем все ресурсы, как только они выполнили свое предназначение
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-
-    glDeleteVertexArrays(1, &VAO1);
-    glDeleteBuffers(1, &VBO1);
+    glDeleteBuffers(1, &EBO);
 
     //Очистка выделенных ресурсов 
     glfwTerminate(); 
